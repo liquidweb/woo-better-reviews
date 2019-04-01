905a9792fd2ce62951d2c355b4193b71d02ec872
Norcross <andrew.norcross@gmail.com>
2019-04-01 10:52:45 -0400

    separating the CSS and JS loads, with optional disable filters

--- includes/front-end.php
+++ includes/front-end.php
@@ -21,7 +21,8 @@ use WP_Error;
  * Start our engines.
  */
 add_action( 'comments_template', __NAMESPACE__ . '\load_review_template', 99 );
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\load_review_front_assets' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\load_review_front_stylesheet' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\load_review_front_javascript' );
 
 /**
  * Load our own review template from the plugin.
@@ -45,11 +46,16 @@ function load_review_template( $default_template ) {
 }
 
 /**
- * Load our front-end side CSS and JS.
+ * Load our front-end side CSS.
  *
  * @return void
  */
-function load_review_front_assets() {
+function load_review_front_stylesheet() {
+
+	// First check if we wanna bypass the entire thing.
+	if ( false !== $disable_css = apply_filters( Core\HOOK_PREFIX . 'disable_stylesheet_load', false ) ) {
+		return;
+	}
 
 	// Bail if we aren't on a single product.
 	if ( ! is_singular( 'product' ) ) {
@@ -76,9 +82,47 @@ function load_review_front_assets() {
 	// Load our CSS file.
 	wp_enqueue_style( $handle, Core\ASSETS_URL . '/css/' . $file . '.css', array( 'dashicons' ), $vers, 'all' );
 
+	// Include our action let others load things.
+	do_action( Core\HOOK_PREFIX . 'after_front_stylesheet_load', $handle );
+}
+
+/**
+ * Load our front-end side JS.
+ *
+ * @return void
+ */
+function load_review_front_javascript() {
+
+	// First check if we wanna bypass the entire thing.
+	if ( false !== $disable_js = apply_filters( Core\HOOK_PREFIX . 'disable_javascript_load', false ) ) {
+		return;
+	}
+
+	// Bail if we aren't on a single product.
+	if ( ! is_singular( 'product' ) ) {
+		return;
+	}
+
+	// Run the check if we're enabled or not.
+	$maybe_enabled  = Helpers\maybe_reviews_enabled( get_the_ID() );
+
+	// Bail if we aren't enabled.
+	if ( ! $maybe_enabled ) {
+		return;
+	}
+
+	// Set my handle.
+	$handle = 'woo-better-reviews-front';
+
+	// Set a file suffix structure based on whether or not we want a minified version.
+	$file   = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? $handle : $handle . '.min';
+
+	// Set a version for whether or not we're debugging.
+	$vers   = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? time() : Core\VERS;
+
 	// And our JS.
 	wp_enqueue_script( $handle, Core\ASSETS_URL . '/js/' . $file . '.js', array( 'jquery' ), $vers, true );
 
 	// Include our action let others load things.
-	do_action( Core\HOOK_PREFIX . 'after_front_assets_load' );
+	do_action( Core\HOOK_PREFIX . 'after_front_javascript_load', $handle );
 }
