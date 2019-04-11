a73b729ade53b600264212c801d4455fa013f009
Norcross <andrew.norcross@gmail.com>
2019-04-11 15:37:45 -0400

    build out the CLI command for the conversion wrapper

--- includes/process/cli-commands.php
+++ includes/process/cli-commands.php
@@ -14,6 +14,7 @@ use LiquidWeb\WooBetterReviews\Helpers as Helpers;
 use LiquidWeb\WooBetterReviews\Utilities as Utilities;
 use LiquidWeb\WooBetterReviews\Queries as Queries;
 use LiquidWeb\WooBetterReviews\Database as Database;
+use LiquidWeb\WooBetterReviews\ConvertExisting as ConvertExisting;
 
 // Pull in the CLI items.
 use WP_CLI;
@@ -244,6 +245,88 @@ class WBR_Commands extends WP_CLI_Command {
 		// Show the result and bail.
 		WP_CLI::success( sprintf( _n( '%d review has been updated.', '%d reviews have been updated.', absint( $update ), 'woo-better-reviews' ), absint( $update ) ) );
 		WP_CLI::halt( 0 );
+	}
+
+	/**
+	 * Convert the existing comment-based reviews.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--cleanup]
+	 * : How to handle the cleanup of the existing.
+	 * ---
+	 * default: convert
+	 * options:
+	 *   - convert
+	 *   - purge
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp woo-better-reviews convert
+	 *
+	 * @when after_wp_load
+	 */
+	function convert( $args = array(), $assoc_args = array() ) {
+
+		// Parse out the associatives.
+		$parse_cli_args = wp_parse_args( $assoc_args, array(
+			'cleanup' => 'convert',
+		));
+
+		// Make sure the cleanup argument is valid.
+		if ( empty( $parse_cli_args['cleanup'] ) || ! in_array( esc_attr( $parse_cli_args['cleanup'] ), array( 'convert', 'purge' ) ) ) {
+			WP_CLI::error( __( 'An invalid cleanup type was requested. Please try again.', 'woo-better-reviews' ) );
+		}
+
+		// Check for the purge and convert flags.
+		$maybe_do_type  = 'convert' === esc_attr( $parse_cli_args['cleanup'] ) ? true : false;
+		$maybe_do_purge = 'purge' === esc_attr( $parse_cli_args['cleanup'] ) ? true : false;
+
+		// The function itself is a single function, since
+		// this is just a wrapper for the conversion function.
+		$maybe_convert  = attempt_existing_review_conversion( $maybe_do_type, $maybe_do_purge );
+
+		// If we have an actual empty return, display
+		// the blank error message and halt.
+		if ( empty( $maybe_convert ) ) {
+			WP_CLI::error( __( 'The attempted conversion returned an empty result.', 'woo-better-reviews' ) );
+		}
+
+		// First look for the 'no-reviews' string. If is
+		// exists, display the 'none' message and halt.
+		if ( 'no-reviews' === esc_attr( $maybe_convert ) ) {
+			WP_CLI::line( __( 'No existing reviews exist to convert.', 'woo-better-reviews' ) );
+			WP_CLI::halt( 0 );
+		}
+
+		// Handle a WP_Error object return.
+		if ( is_wp_error( $maybe_convert ) ) {
+
+			// Get my error code and message.
+			$wp_error_code  = $maybe_convert->get_error_code();
+			$wp_error_text  = $maybe_convert->get_error_message();
+
+			// Build my return message.
+			$return_message = sprintf( __( '%1$s | ERR_CODE %2$s', 'woo-better-reviews' ), esc_attr( $error_text ), esc_attr( $error_code ) );
+
+			// Display the formatted error message.
+			WP_CLI::error( esc_attr( $return_message ) );
+		}
+
+		// If we have the success flag, return that.
+		if ( ! empty( $maybe_convert['success'] ) ) {
+
+			// Determine the message.
+			$return_message = ! empty( $maybe_convert['message'] ) ? $maybe_convert['message'] : __( 'The existing reviews have been converted.', 'woo-better-reviews' );
+
+			// Show the result and bail.
+			WP_CLI::success( esc_attr( $return_message ) );
+			WP_CLI::halt( 0 );
+		}
+
+		// Display our "unknown" error.
+		WP_CLI::error( __( 'The conversion has completed but there was no results returned. Please check the status manually.', 'woo-better-reviews' ) );
 	}
 
 	/**
