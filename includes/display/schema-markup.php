c6f8b35c7fea5208923f438010f58c168330fc20
Norcross <andrew.norcross@gmail.com>
2019-04-12 12:05:26 -0400

    adding schema file for beginning of structure.

---
+++ includes/display/schema-markup.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Construct and render the various schema inserts.
+ *
+ * @package WooBetterReviews
+ */
+
+// Declare our namespace.
+namespace LiquidWeb\WooBetterReviews\SchemaMarkup;
+
+// Set our aliases.
+use LiquidWeb\WooBetterReviews as Core;
