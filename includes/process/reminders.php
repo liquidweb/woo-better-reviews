66d8f5489708e1570b456c40507f4f9a67445e4a
Norcross <andrew.norcross@gmail.com>
2019-05-13 13:13:30 -0400

    adding the removal after an email is sent

--- includes/process/reminders.php
+++ includes/process/reminders.php
@@ -21,7 +21,7 @@ use WP_Error;
  * Start our engines.
  */
 add_action( 'wc_better_reviews_trigger_after_purchase_order_line_items', __NAMESPACE__ . '\maybe_set_reminder_at_order', 10, 3 );
-add_action( 'wc_better_reviews_trigger_status_change_order_completed', __NAMESPACE__ . '\maybe_set_reminder_at_completed', 10, 3 );
+add_action( 'wc_better_reviews_trigger_status_change_order_completed', __NAMESPACE__ . '\maybe_set_reminder_at_completed', 10, 2 );
 
 /**
  * Check to see if we need to set reminders for purchased products.
@@ -183,3 +183,103 @@ function maybe_set_reminder_at_completed( $order_id, $order_data ) {
 	do_action( Core\HOOK_PREFIX . 'after_status_completed_reminder_set', $reminder_arr, $order_id, $product_ids, $order_data );
 }
 
+/**
+ * Remove the product ID from the array of pending items.
+ *
+ * @param  integer $order_id      The order ID we just sent the email from.
+ * @param  array   $product_list  The product IDs contained in the email.
+ *
+ * @return mixed
+ */
+function remove_completed_reminders( $order_id = 0, $product_list = array() ) {
+
+	// Bail if no data.
+	if ( empty( $order_id ) || empty( $product_list ) ) {
+		return;
+	}
+
+	// Pull out the entire meta group.
+	$reminder_meta  = get_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_data', true );
+
+	// If we don't have any (odd) then changed the other key and bail.
+	if ( empty( $reminder_meta ) ) {
+
+		// Change the status.
+		update_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_status', 'empty' );
+
+		// Delete the dataset key all together.
+		delete_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_data' );
+
+		// Purge the meta.
+		Utilities\purge_order_reminder_meta( $order_id );
+
+		// And return.
+		return;
+	}
+
+	// Pull out the product list, with our timestamps.
+	$product_group  = wp_list_pluck( $reminder_meta, 'timestamp', 'product_id' );
+
+	// Compare the lists. If none remain, then we are done here.
+	$list_compare   = array_diff( $product_list, array_keys( $product_group ) );
+
+	// None left! Clean plate club!
+	if ( empty( $list_compare ) ) {
+
+		// Change the status.
+		update_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_status', 'completed' );
+
+		// Delete the dataset key all together.
+		delete_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_data' );
+
+		// Purge the meta.
+		Utilities\purge_order_reminder_meta( $order_id );
+
+		// And return.
+		return;
+	}
+
+	// Now set an empty for our remaining array.
+	$update_meta    = array();
+
+	// Loop the array of IDs and
+	foreach ( $product_group as $product_id => $timestamp ) {
+
+		// If we have a match, add to our array.
+		if ( in_array( $product_id, $list_compare ) ) {
+
+			// Add my product ID and the date stamp.
+			$update_meta[]  = array(
+				'product_id' => absint( $product_id ),
+				'timestamp'  => absint( $timestamp ),
+			);
+		}
+
+		// Nothing left inside this loop.
+	}
+
+	// None left! Clean plate club!
+	if ( empty( $update_meta ) ) {
+
+		// Change the status.
+		update_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_status', 'completed' );
+
+		// Delete the dataset key all together.
+		delete_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_data' );
+
+		// Purge the meta.
+		Utilities\purge_order_reminder_meta( $order_id );
+
+		// And return.
+		return;
+	}
+
+	// Make sure it's still set to pending
+	update_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_status', 'pending' );
+
+	// Update the meta with what we have left.
+	update_post_meta( $order_id, Core\META_PREFIX . 'review_reminder_data', $update_meta );
+
+	// And be done.
+	return;
+}
